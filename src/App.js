c197082b86a55baeb932926472ab247106594baa
sauravjadhav <86429347+sauravjadhav@users.noreply.github.com>
2023-01-13 00:03:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Banner from "./Banner";
+import Form from "./Form";
+import Footer from "./Footer";
+
+const App = () => {
+  return (
+    <>
+      <Banner />
+      <Form />
+      <Footer />
+    </>
+  );
+};
+export default App;
